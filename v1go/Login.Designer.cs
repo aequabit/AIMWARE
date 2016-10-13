493609654e85d72a497319edc0e570b8abe61e0a
aequabit <contact@aequabit.de>
2016-10-13 04:18:46 +0200

    Set btnLogin as AcceptButton of Login

--- v1go/Login.Designer.cs
+++ v1go/Login.Designer.cs
@@ -115,6 +115,7 @@
             // 
             // Login
             // 
+            this.AcceptButton = this.btnLogin;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(327, 234);
